9cf2d7a161930772a85362e55ce2f81193df164e
plonxyz <egon.lampert@gmail.com>
2023-09-30 18:36:12 +0200

    Update pcap-vt.py
    added progressbar

--- pcap-vt.py
+++ pcap-vt.py
@@ -1,15 +1,17 @@
 import pyshark
 import requests
+from tqdm import tqdm
 import csv
 
 VIRUSTOTAL_URL = "https://www.virustotal.com/api/v3/ip_addresses/{}"
-API_KEY = "YOUR_API_KEY"  # Replace with your VirusTotal API key
+API_KEY = "YOUR_VIRUSTOTAL_API_KEY"
+
 
 def extract_destination_ips(pcap_file):
     cap = pyshark.FileCapture(pcap_file)
     dest_ips_set = set()
- 
-    for packet in cap:
+
+    for packet in tqdm(cap, desc="Extracting IPs from pcap", unit="packet"):
         if 'IP' in packet:
             dest_ips_set.add(packet.ip.dst)
 
@@ -28,7 +30,6 @@ def check_ip_virustotal(ip):
         undetected = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("undetected", 0)
         return [ip, malicious, suspicious, harmless, undetected]
     else:
-        print(f"Error checking IP {ip}. Status Code: {response.status_code}")
         return [ip, "Error", "Error", "Error", "Error"]
 
 if __name__ == "__main__":
@@ -36,11 +37,11 @@ if __name__ == "__main__":
     ips_list = extract_destination_ips(pcap_path)
     results = []
 
-    for ip in ips_list:
+    print("Processing IPs...")
+    for ip in tqdm(ips_list, desc="Checking IPs against VirusTotal"):
         result = check_ip_virustotal(ip)
         results.append(result)
 
-    # Write results to CSV
     with open('results.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(["IP", "Malicious", "Suspicious", "Harmless", "Undetected"])
